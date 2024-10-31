e6ae4aaa47d929fe12b4c6f8c1b84f9da73eaf0c
r7projects-shayan <shayansworks@gmail.com>
2024-11-01 02:22:12 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
 import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+  throw new Error("Missing Clerk Publishable Key");
 }
 
 createRoot(document.getElementById("root")).render(
